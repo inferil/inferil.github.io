eca68438dbdbc12903d7e1e944fa403d87e33f0f
inferil <brunomoura.bm2007@gmail.com>
2026-05-18 14:41:17 +0100

    Footer changes

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const year = new Date().getFullYear();
-document.getElementById('footer').textContent = year;
+const footer = document.getElementById('footer');
+
+const year = document.createElement('span');
+year.textContent = new Date().getFullYear();
+
+const quote = document.createElement('span');
+quote.textContent = ' • O que sobrou de nós?';
+quote.classList.add('quote');
+
+footer.appendChild(year);
+footer.appendChild(quote);
 
 const md = document.getElementById('md').textContent;
 document.getElementById('conteudo').innerHTML = marked.parse(md);
